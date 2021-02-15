1bb40121a658d068745be70cc947357798e611d3
Farid Huseynov <faridhuseynov.eng@gmail.com>
2021-02-15 13:05:36 +0400

    x-Axis set up but throws error

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const url =
   "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json";
+
+const height = 700;
+const width = 800;
+const padding = 80;
+
 var xhttp = new XMLHttpRequest();
 xhttp.open("GET", url, true);
 xhttp.send();
@@ -8,9 +13,27 @@ xhttp.onload = () => {
   console.log(dataset);
 
   //title element with id="title"
-  d3.select("body").append("title")
-  .attr("id", "title")
-  .text("Scatter Plot");
+  d3.select("body").append("title").attr("id", "title").text("Scatter Plot");
 
+  const years = dataset.map((data) => data.Year);
+
+  const xScale = d3
+    .scaleLinear()
+    .domain(0, d3.max(years))
+    .range(height, height - padding);
+
+  const xAxis = d3.axisBottom(xScale);
+
+  const svg = d3
+    .select("body")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+  svg
+    .append("g")
+    .attr("transform", "translate(0,"
+     + (height - padding) + ")")
+    .call(xAxis);
 
 };
